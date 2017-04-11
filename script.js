e40c107e3a62be6f2df185a2e1a9dcb522113aa2
fsorodrigues <desouzaoliveirarod.f@husky.neu.edu>
2017-04-11 17:37:17 -0400

    Committing 2nd iteration
    d3-tip
    calling tooltip
    sorting values by TDR
    wrapping condition for stacking the <rect>
    comments

--- script.js
+++ script.js
@@ -8,22 +8,24 @@ var stacking = unit*1.1;
 var svg = d3.select(".content")
             .append("svg");
 
-var tool_tip = d3.tip()
-      .attr("class", "d3-tip")
-      .offset([0, 50])
-      .html(function(d) { var tooltipText = "<b>" + d.Date + "</b> vs. " + d.Opp + "</b>"
-                                            + "<br> <b>P:</b> " + d.PTS
-                                            + " <b>R:</b> " + d.TRB
-                                            + " <b>A:</b> " + d.AST
-                                            + "<br> <b>TDR:</b> " + d.TDR ;
+var tool_tip = d3.tip() //setting the tool_tip <div> with d3-tip
+                 .attr("class", "d3-tip")
+                 .offset([0, 50])
+                 .html(function(d) { var tooltipText = "<b>" + d.Date + "</b> vs. " + d.Opp + "</b>"
+                                                        + "<br> <b>P:</b> " + d.PTS
+                                                        + " <b>R:</b> " + d.TRB
+                                                        + " <b>A:</b> " + d.AST
+                                                        + "<br> <b>TDR:</b> " + d.TDR ;
 
-                               return tooltipText; });
-    svg.call(tool_tip);
+                                         return tooltipText; });
 
-d3.csv("triple-doubles_8384_to1617_apr_3rd - raw_data_8384_to_1617_apr_3rd.csv", function(error, dataset) {
+    svg.call(tool_tip); //calling tool_tip in svg context
+
+d3.csv("triple-doubles_8384_to1617_apr_3rd - raw_data_8384_to_1617_apr_3rd.csv", function(error, dataset) { //setting callback csv function
 
   var nested_dataset = d3.nest()
-                       .key(function(d) { return d.Player; })
+                       .key(function(d) { return d.Player; }) //nesting by Player. To sort A-Z add: .sortKeys(d3.ascending)
+                       .sortValues(function(a,b) { return parseFloat(a.TDR) - parseFloat(b.TDR); } ) //sorting ascending by TDR
                        .entries(dataset);
 
   // console.log(nested_dataset);
@@ -34,6 +36,7 @@ d3.csv("triple-doubles_8384_to1617_apr_3rd - raw_data_8384_to_1617_apr_3rd.csv",
   //
   // console.log(count_nested_dataset);
 
+  //Creating players
   var players = svg.selectAll("g")
                    .data(nested_dataset)
                    .enter()
@@ -42,33 +45,38 @@ d3.csv("triple-doubles_8384_to1617_apr_3rd - raw_data_8384_to_1617_apr_3rd.csv",
                    .attr('transform', function(d, i) {
                                        return "translate(0," + i * 50 + ")"; });
 
+ //Printing player name
  var label = players.append("text")
                     .attr("class", "label")
                     .text(function(d) { return d.key; });
 
+ //Printing total triple-doubles
  var printTD = players.append("text")
                       .attr("class", "label2")
                       .text(function(d) { return d.values.length + " total triple-doubles"; })
-                      .attr('dx', 190);
+                      .attr('dx', 150);
 
-  var timeline =  players.append("g")
+ var timeline =  players.append("g")
                          .attr("class", "timeline");
 
-  var entries = timeline.selectAll(".entries")
-                       .data(function (d) { return d.values; })
+ //Appending each triple-double as a rect
+ var entries = timeline.selectAll(".entries")
+                       .data(function (d) { return d.values; }) //Acessing nested values
                        .enter()
                        .append("rect")
                        .attr("class", "entries")
                        .attr("width", rectW)
                        .attr("height", rectH)
                        .style("fill", "#c13e3e")
+                       //Hover animation
                        .on('mouseover', function(d) { tool_tip.attr('class', 'd3-tip animate').show(d)
                                                       d3.select(this).transition().duration(200).style("fill", "hsla(0,0%,0%,0.7)"); })
                        .on('mouseout', function(d) { tool_tip.attr('class', 'd3-tip').show(d)
                                                      tool_tip.hide()
                                                      d3.select(this).transition().duration(0).style("fill", "#c13e3e"); });
 
-  var stackedRect = entries.attr("x", function(d,i) { return i * stacking; })
-                           .attr("y", 8);
+ //Stacking rect, conditionals to stack in next line
+ var stackedRect = entries.attr("x", function(d,i) { if (i >= 60) { return ((i - 60) * stacking) } else { return i * stacking }; })
+                           .attr("y", function(d,i) { if (i >= 60) { return 17 } else { return 8 }; });
 
-});
+}); //closing csv function
